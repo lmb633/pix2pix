3bf2d960167bbaa8e6adf97489bc4e6c14cb4f7c
李明博E0012960 <limingbo@focusmedia.cn>
2019-12-17 19:36:18 +0800

    reconsitution

--- train2.py
+++ train2.py
@@ -5,10 +5,12 @@ from data_gen import DatasetFromFolder
 from os.path import join
 import os
 from model import define_G, define_D, GANLoss
+from utils import AverageMeter
 
 root = 'data/cityscapes/'
 mode = 'a2b'
 
+print_freq = 10
 weight = 10
 epochs = 1000
 lr = 0.0001
@@ -43,6 +45,10 @@ optimzer_g = torch.optim.Adam(net_g.parameters(), lr)
 optimzer_d = torch.optim.Adam(net_d.parameters(), lr)
 
 for epoch in range(epochs):
+    avg_loss_g = AverageMeter()
+    avg_loss_d = AverageMeter()
+    min_loss_g = float('inf')
+    min_loss_d = float('inf')
     for i, data in enumerate(train_loader):
         img_a, img_b = data[0].to(device), data[1].to(device)
         fake_b = net_g(img_a)
@@ -70,6 +76,14 @@ for epoch in range(epochs):
         loss_g = criterionGAN(out_fake, True)
 
         loss_L1 = criterionL1(img_b, fake_b) * weight
-        loss = loss_g + loss_L1
-        loss.backward()
+        loss_g = loss_g + loss_L1
+        loss_g.backward()
         optimzer_g.step()
+
+        avg_loss_d.update(loss_d)
+        avg_loss_g.update(loss_g)
+
+        if i % print_freq == 0:
+            print('loss: lossg {0} lossd{1} avgg{2} avgd{3}'.format(avg_loss_g.val, avg_loss_d.val, avg_loss_g.avg, avg_loss_d.avg))
+        if loss_g < min_loss_g and loss_d < min_loss_d:
+            torch.save((net_g, net_d), check)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-
+import torch
 
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
@@ -20,3 +20,38 @@ def save_img(image_tensor, filename):
     image_pil = Image.fromarray(image_numpy)
     image_pil.save(filename)
     print("Image saved as {}".format(filename))
+
+
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def save_checkpoint(epoch, epochs_since_improvement, model, optimizer, hmean, is_best):
+    state = {'epoch': epoch,
+             'epochs_since_improvement': epochs_since_improvement,
+             'hmean': hmean,
+             'model': model,
+             'optimizer': optimizer}
+    # filename = 'checkpoint_' + str(epoch) + '_' + str(loss) + '.tar'
+    filename = 'checkpoint.tar'
+    torch.save(state, filename)
+    # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
+    if is_best:
+        torch.save(state, 'BEST_checkpoint.tar')
